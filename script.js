19e2759b94561713238c6d4a998138b3dee4df95
Cindy <cindydunham4@gmail.com>
2021-12-19 19:14:26 -0500

    adding in variables

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Assignment code here
+//generate element by ID, arrays of data
+var generateBtn=document.getElementById("generate")
+var upperCase=["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase=["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var number=["0","1","2","3","4", "5", "6", "7", "8", "9"]
+var specialCharactors=["!","@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", ":"]
+var arrayofAcceptableValues=[]
+var length=0
+var passWord=""
+var charactorQuantity=""
+var useUpperCase=""
+var useLowerCase=""
+var useNumbers=""
+var useSpecialCharactors=""
+
 
 
 
